d072ab02ee96d0a4d4d6ef4641c20952d2d0a779
szabomarcel <szabomarcell34@gmail.com>
2024-05-06 16:07:07 +0200

    gyakorlofeladat

---
+++ ConsoleAppDolgozok/Database.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleAppDolgozok
+{
+    internal class Database
+    {
+        MySqlCommand sqlCommand;
+        MySqlConnection connection;
+        public Database()
+        {
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Server = "localhost";
+            builder.UserID = "root";
+            builder.Password = "";
+            builder.Database = "dolgozok";
+            connection = new MySqlConnection(builder.ConnectionString);
+            sqlCommand = connection.CreateCommand();
+            try
+            {
+                kapcsolatnyit();
+                kapcsolatzar();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+        }
+        private void kapcsolatzar()
+        {
+            if (connection.State != System.Data.ConnectionState.Closed)
+            {
+                connection.Close();
+            }
+        }
+
+        private void kapcsolatnyit()
+        {
+            if (connection.State != System.Data.ConnectionState.Open)
+            {
+                connection.Open();
+            }
+        }
+    }
+}
---
+++ ConsoleAppDolgozok/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDolgozok
+{
+    internal class Program
+    {
+        static Database db = new Database();
+        static List<Dolgozok> elemek = new List<Dolgozok>();
+        static List<Dolgozok> Topfizetes = new List<Dolgozok>();
+        static List<Dolgozok> munkakor = new List<Dolgozok>();
+        static void Main(string[] args)
+        {
+            feladat01();
+            feladat02();
+            feladat03();
+            Console.WriteLine("\nProgram vége");
+            Console.ReadLine();
+        }
+
+        private static void feladat03()
+        {
+            Console.WriteLine("\n3. feladat");
+            // Munkakörök számolása
+            var munkakorokSzama = munkakor.GroupBy(d => d.reszleg).Select(group => new { Munkakor = group.Key, Darab = group.Count() });
+            foreach (var munkakor in munkakorokSzama)
+            {
+                Console.WriteLine($"Munkakör: {munkakor.Munkakor}, Dolgozók száma: {munkakor.Darab}");
+            }
+        }
+
+        private static void feladat02()
+        {
+            Console.WriteLine("\n2. feladat");
+            if (Topfizetes.Count > 0)
+            {
+                // Kiírjuk a legmagasabb fizetéssel rendelkező dolgozó nevét
+                var legmagasabbFizetes = Topfizetes.OrderByDescending(d => d.ber).First();
+                Console.WriteLine($"Legmagasabb fizetéssel rendelkező dolgozó: {legmagasabbFizetes.nev}");
+            }
+            else
+            {
+                Console.WriteLine("Nincs adat a legmagasabb fizetéssel rendelkező dolgozóról.");
+            }
+        }
+
+        private static void feladat01()
+        {
+            Console.WriteLine("1. feladat");
+            // Kiírjuk az elemek számát
+            Console.WriteLine($"Elemek száma: {elemek.Count}");
+        }
+    }
+}
